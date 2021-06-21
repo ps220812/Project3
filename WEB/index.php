caaf2ee2562e3630e3816eade41616eef47034f3
ps195415 <ps195415@summacollege.nl>
2021-06-21 09:57:26 +0200

    Web Home page bijgewerkt

---
+++ WEB/index.php
@@ -0,0 +1,53 @@
+<?php
+include_once "./classes/VerkiezingenDB.php";
+$db = new VerkiezingenDB();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/menu.css">
+</head>
+
+<body>
+    <header>
+        <div class="container-header">
+            <div class="topnav">
+                <nav>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="themas.php">Themas</a></li>
+                        <li><a href="kieswijzer.php">Stem</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-1">
+                    <h1>Stemwijzer</h1>
+                    <p class="pText">Welkom op de stemwijzer. Deze pagina is bedoelt om u te helpen met het maken van uw keuze tijdens de verkiezingen.</p>
+                </div>
+                <div class="col-2">
+                    <button class="btnStart">Start</button>
+                </div>
+            </div>
+        </div>
+    </main>
+
+
+
+
+
+
+</body>
+
+</html>
