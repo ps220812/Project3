87e85ffaf762c721b66873ba8e6a62be9741a8c6
ps220812 <ps220812@summacollege.nl>
2021-06-21 10:24:39 +0200

    Select functie php

--- WEB/classes/VerkiezingenDB.php
+++ WEB/classes/VerkiezingenDB.php
@@ -4,5 +4,18 @@
         const DSN = "mysql:host=localhost;dbname=verkiezingenprj3";
         const USER = "root";
         const PASSWD = "";
+
+        function selectPartijen(){
+            try{
+                $pdo = new PDO(self::DSN,self::USER,self::PASSWD);
+                $statement = $pdo->prepare("SELECT*FROM partij;");
+                $statement->execute();
+                $rows = $statement->fetchall(PDO::FETCH_ASSOC);
+                return $rows;
+            }
+            catch(PDOException $e){
+                return false;
+            }
+        }
     }
 ?>
